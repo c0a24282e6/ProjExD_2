4ca615b2293f36edefe3940acc815ff750c2fb32
Warabi <c0a24282e6@edu.teu.ac.jp>
2025-04-22 17:29:20 +0900

    2.時間とともに爆弾が拡大、加速する

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -45,18 +45,35 @@ def gameover(screen: pg.Surface) -> None:
     screen.blit(penkouka_img,[780,240])
     pg.display.update()
 
+def init_bb_imgs() -> tuple[list[pg.Surface],list[int]]:
+
+    list=[]
+
+    sbb_accs= [a for a in range(1, 11)]
+
+    for r in range(1, 11):
+        bb_img= pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        list.append(bb_img)
+    
+    return list,sbb_accs
+
+
 
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+
     # こうかとん初期化
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
     # 爆弾初期化
+    bb_imgs, bb_accs = init_bb_imgs()
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
@@ -68,6 +85,12 @@ def main():
     clock = pg.time.Clock()
     tmr = 0
     while True:
+
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vx*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_img.set_colorkey((0, 0, 0))
+
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
@@ -78,7 +101,6 @@ def main():
             gameover(screen)
             time.sleep(5)
             return
-            
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -87,19 +109,11 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]  # 左右方向
                 sum_mv[1] += mv[1]  # 上下方向
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True): # 画面外だったら
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 画面内に戻す
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)  # 爆弾の移動
+        bb_rct.move_ip(avx, vy)  # 爆弾の移動
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 左右どちらかにはみ出ていたら
             vx *= -1
